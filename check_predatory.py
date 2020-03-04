ed26e58bc843c1cd17ac9144ea72e69efd475be8
bganglia <bganglia892@gmail.com>
2020-03-04 16:18:29 -0500

    framework for class to check whether a journal is predatory

---
+++ check_predatory.py
@@ -0,0 +1,9 @@
+class PredatoryInfo():
+     def __init__(self):
+         pass
+     def _load(self):
+         pass
+     def update(self):
+         pass
+     def is_predatory(self, name):
+         pass
