edd692bebf2b2148c5a971b78d3e506a4fa8bcad
bganglia <bganglia892@gmail.com>
2020-03-04 16:26:52 -0500

    add search method

--- check_predatory.py
+++ check_predatory.py
@@ -9,8 +9,13 @@ class PredatoryInfo():
              self._data = json.load(handle)
      def update(self):
          pass
-     def search(self):
-         pass
+     def search(self, name):
+         for listing in self._data:
+             if matches_domain(name, listing["domain"]):
+                 return listing
+             if matches_name(name, listing["name"]):
+                 return listing
+         return {}
      def is_predatory(self, name):
          result = self.search(name)
          if result:
