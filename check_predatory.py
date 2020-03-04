128da13e2207c0fcf2f0129d3e7371638f2244a7
bganglia <bganglia892@gmail.com>
2020-03-04 16:21:37 -0500

    add _load method

--- check_predatory.py
+++ check_predatory.py
@@ -1,9 +1,12 @@
+import json
+
 class PredatoryInfo():
      self.predatory_path = ""
      def __init__(self):
          self._load(predatory_path)
      def _load(self):
-         pass
+         with open(self.predatory_path) as handle:
+             self._data = json.load(handle)
      def update(self):
          pass
      def is_predatory(self, name):
