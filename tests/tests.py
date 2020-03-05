5c70e463b7db0176e1ca84774280919b5581c09a
bganglia <bganglia892@gmail.com>
2020-03-05 11:37:50 -0500

    fix test

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
-import check_predatory
+import is_this_journal_predatory
 
 def test_check_predatory():
-    predatory_checker = check_predatory.PredatoryInfo()
+    predatory_checker = is_this_journal_predatory.PredatoryInfo()
     returned_message = predatory_checker.is_predatory("Academic Journals")
-    expected_message = 
-    assert returned_message = expected_message
+    expected_message = "The journal Academic Journals at http://www.academicjournals.org/ is listed as a predatory journal in Beall's List."
+    assert returned_message == expected_message
