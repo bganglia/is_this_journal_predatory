12d09ae33394e04b57e9f68c0803aa95e72652bf
bganglia <bganglia892@gmail.com>
2020-03-05 12:31:03 -0500

    use include_package_data instead of data_files

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setuptools.setup(
     description="Check whether a journal is listed in Beall's List from within Python.",
     long_description="",
     long_description_content_type="text/markdown",
-    data_files=[("is_this_journal_predatory",["is_this_journal_predatory/predatory_journals.json"])],
     url="https://github.com/bganglia/is_this_journal_predatory/blob/master/check_predatory.py",
     packages=setuptools.find_packages(),
     classifiers=[], #fill in
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    include_package_data=True
 )
