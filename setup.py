f102f635994cf9c3ec4d81d3d2b7e6e25daa8bd7
bganglia <bganglia892@gmail.com>
2020-03-05 12:13:41 -0500

    use data_files parameter correctly

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description="Check whether a journal is listed in Beall's List from within Python.",
     long_description="",
     long_description_content_type="text/markdown",
-    data_files=[("is_this_journal_predatory","is_this_journal_predatory/predatory_journals.json")],
+    data_files=[("is_this_journal_predatory",["is_this_journal_predatory/predatory_journals.json"])],
     url="https://github.com/bganglia/is_this_journal_predatory/blob/master/check_predatory.py",
     packages=setuptools.find_packages(),
     classifiers=[], #fill in
