8a90ef82444792d4ed0ada23956091f037bbc4d2
bganglia <bganglia892@gmail.com>
2020-03-05 11:32:47 -0500

    Track other files with git

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="is_this_journal_predatory",
+    version="0.0.1",
+    author="bganglia",
+    author_email="bganglia892@gmail.com",
+    description="Check whether a journal is listed in Beall's List from within Python.",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/bganglia/is_this_journal_predatory/blob/master/check_predatory.py",
+    packages=setuptools.find_packages(),
+    classifiers=[], #fill in
+    python_requires=">=3.6"
+)
---
+++ tests/tests.py
@@ -0,0 +1,7 @@
+import check_predatory
+
+def test_check_predatory():
+    predatory_checker = check_predatory.PredatoryInfo()
+    returned_message = predatory_checker.is_predatory("Academic Journals")
+    expected_message = 
+    assert returned_message = expected_message
